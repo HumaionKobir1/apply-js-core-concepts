d7ccb725f4d896c49c7b7074ccb04d9e26d8309b
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-02 16:34:44 +0600

    Added => factorial for while loop

---
+++ factorial-while.js
@@ -0,0 +1,12 @@
+function factorial (number){
+    let num = 1;
+    let result = 1;
+    while (num <= number){
+        result = result * num;
+        num ++;
+    }
+    return result;
+}
+const number = 5;
+const fact = factorial(number);
+console.log(fact);
