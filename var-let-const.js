f01feb16466b9b8c625d3fd3cd1cea00b627452f
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-02 11:58:27 +0600

    Added => let and const difference

---
+++ var-let-const.js
@@ -0,0 +1,27 @@
+let age = 11;
+age = 12;
+console.log(age);
+
+
+
+let greeting = 'say Hi';
+let times = 4;
+
+if (times > 3) {
+    let greeting = 'say Hello instead';
+    console.log(greeting);
+}
+console.log(greeting);
+
+
+
+
+const greetings = "say Hi";
+    greetings = "say Hello instead"; // error: Assignment to constant variable. 
+
+    // const greetings2 = "say Hi";
+    // const greetings2 = "say Hello instead"; // error: Identifier 'greeting' has already been declared
+
+    // This behavior is somehow different when it comes to objects declared with const. 
+
+    
